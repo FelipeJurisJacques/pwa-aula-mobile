37d20a9dc56a04ad5747d3ab2414cfd449b70e6a
FelipeJurisJacques <40221243+FelipeJurisJacques@users.noreply.github.com>
2019-03-18 21:14:52 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,3 +3,11 @@ const PRECACHE = 'precache-v1'
 const PRECACHE_URLS = [
     'index.html'
 ]
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(PRECACHE)
+        .then(Cache => Cache.addAll(PRECACHE_URLS))
+        .then(self.skipWaiting())
+    )
+})
